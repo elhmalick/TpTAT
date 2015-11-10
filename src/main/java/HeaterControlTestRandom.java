e079d79ee69ea700c143082a656f54d7c78dd785
malick DIAGNE <milkzodiagne@yahoo.fr>
2015-11-10 16:54:39 +0100

    add some file

--- src/main/java/HeaterControlTestRandom.java
+++ src/main/java/HeaterControlTestRandom.java
@@ -5,8 +5,12 @@ import java.util.Random;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+
+@RunWith(MockitoJUnitRunner.class)
 public class HeaterControlTestRandom {
 	private Random r;
 
@@ -22,25 +26,41 @@ public class HeaterControlTestRandom {
 	@Before
 	public void setUp() {
 		this.r = new Random();
-		// XXX
+		hc = new HeaterControl(21, s1, s2, s3, true);
 	}
 
 	@Test
 	public void test_alea1() {
 		double t1, t2, t3;
-		// XXX
-		double[] t_list = new double[] { 16, 17, 18, 19, 20, 21, 22, 21, 20,19, 18, 17, 16 };
+		double oldTemp=0;
+		double newTemp;
+		boolean oldState;
+		double[] t_list = new double[] { 16, 17, 18, 19, 20, 21, 22, 21,20,19,18 };
+
+		int i=0;
 		for (double t : t_list) {
+			if(i==0)
+				oldTemp = t;
+			newTemp = t;
+			oldState = hc.getOn();
+
 			t1 = t + r.nextDouble() - 0.5;
 			t2 = t + r.nextDouble() - 0.5;
 			t3 = t + r.nextDouble() - 0.5;
 			when(s1.getT()).thenReturn(t1);
 			when(s2.getT()).thenReturn(t2);
 			when(s3.getT()).thenReturn(t3);
-			hc = new HeaterControl(25, s1, s2, s3, false);
 			hc.step();
-			assertEquals(hc.getOn(), true);
+
+			if ((newTemp > oldTemp) && (newTemp < hc.getTc()))
+				assertEquals(hc.getOn(), oldState);
+			
+			else if(newTemp > hc.getTc())
+				assertEquals(hc.getOn(), !oldState); 
+
 			System.out.printf("test alea 1: t=%f  \n", t);
+			oldTemp = newTemp;
+			i++;
 		}
 	}
 }
